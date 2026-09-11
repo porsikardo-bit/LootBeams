efa89bda0c3ef5fffda59fb1174cc15e0a6a56e4
porsikardo-bit <porsikardo@gmail.com>
2026-09-10 22:14:19 -0600

    Update Configuration.java

--- src/main/java/com/lootbeams/Configuration.java
+++ src/main/java/com/lootbeams/Configuration.java
@@ -54,7 +54,7 @@ public class Configuration {
 		BEAM_HEIGHT = clientBuilder.comment("The height of the Loot Beam.").defineInRange("beam_height", 1D, 0D, 10D);
 		BEAM_Y_OFFSET = clientBuilder.comment("The Y-offset of the loot beam.").defineInRange("beam_y_offset", 0D, -30D, 30D);
 		BEAM_ALPHA = clientBuilder.comment("Transparency of the Loot Beam.").defineInRange("beam_alpha", 0.85D, 0D, 1D);
-		RENDER_DISTANCE = clientBuilder.comment("How close the player has to be to see the beam. (note: ItemEntities stop rendering at 24 blocks, so that is the limit for beams)").defineInRange("render_distance", 24D, 0D, 24D);
+		RENDER_DISTANCE = clientBuilder.comment("How close the player has to be to see the beam. Increased limit for custom distance rendering.").defineInRange("render_distance", 32D, 0D, 64D);
 		COLOR_OVERRIDES = clientBuilder.comment("Overrides an item's beam color with hex color. Must follow the specific format: (registryname=hexcolor) Or (#tagname=hexcolor). Example: \"minecraft:stone=0xFFFFFF\". This also accepts modids.").define("color_overrides", new ArrayList<>());
 
 		clientBuilder.comment("Item Config").push("Items");
